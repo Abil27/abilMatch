e0b82d537b0c085edf74c14203482ee37e090fb0
Houssein Abil <milameet@gmail.com>
2018-11-13 00:28:40 +0000

    savepoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   #get 'contacts/new'
   get 'about', to: 'pages#about'
-  get 'contact-us', to: 'contacts#new'
-  resources :contacts
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  resources :contacts, only: :create
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
